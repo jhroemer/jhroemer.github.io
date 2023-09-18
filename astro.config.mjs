6c684835ed5f0ae92c6122f5ecbc079c7dc9f125
Jens Rømer Hesselbjerg <jh.roemer@gmail.com>
2023-09-18 18:10:35 +0200

    Remove redundant Preact component and Preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()]
+  integrations: [mdx()],
 });
