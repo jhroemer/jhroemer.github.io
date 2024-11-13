30739f44326007440266afb9fa4f5c1abd3f048c
Jens Rømer Hesselbjerg <jh.roemer@gmail.com>
2024-11-13 13:31:33 +0100

    Add visual regression testing (#154)
    * Install playwright
    
    * Add visual regression tests
    
    * Add visual regression testing to CI
    
    * Fix build script issue
    
    * Structure tests differently to prevent timeout's
    
    * Tests run with same environment locally as in CI
    
    * Only test with chromium for now due to flake issue

---
+++ tests/visual.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "@playwright/test";
+
+test("Root page", async ({ page }) => {
+  await page.emulateMedia({ colorScheme: "light" });
+
+  await page.goto("/", {
+    waitUntil: "networkidle",
+  });
+
+  await expect(page).toHaveScreenshot("root-light.png");
+
+  await page.emulateMedia({ colorScheme: "dark" });
+
+  await expect(page).toHaveScreenshot("root-dark.png");
+});
+
+test("About page", async ({ page }) => {
+  await page.emulateMedia({ colorScheme: "light" });
+
+  await page.goto("/about", {
+    waitUntil: "networkidle",
+  });
+
+  await expect(page).toHaveScreenshot("about-light.png");
+
+  await page.emulateMedia({ colorScheme: "dark" });
+
+  await expect(page).toHaveScreenshot("about-dark.png");
+});
+
+test("Blog page", async ({ page }) => {
+  await page.emulateMedia({ colorScheme: "light" });
+
+  await page.goto("/blog", {
+    waitUntil: "networkidle",
+  });
+
+  await expect(page).toHaveScreenshot("blog-light.png");
+
+  await page.emulateMedia({ colorScheme: "dark" });
+
+  await expect(page).toHaveScreenshot("blog-dark.png");
+});
