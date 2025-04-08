1fde488884b14b6495ff7047aeb0118d065f9bf8
Jens Rømer Hesselbjerg <jh.roemer@gmail.com>
2025-04-08 10:39:50 +0200

    Add test coverage

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -1,4 +1,18 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, type Page } from "@playwright/test";
+import GET_https_public_api_bsky_app_xrpc_app_bsky_feed_getPo_post_2F3ljphoefymc2l_ZS1KSLN from "./mocks/GET_https_public.api.bsky.app_xrpc_app.bsky.feed.getPo.post%2F3ljphoefymc2l_ZS1KSLN";
+
+export const setupFixtures = async (
+  page: Page,
+  fixtures: { url: string; response: unknown }[]
+) => {
+  await Promise.all(
+    fixtures.map(async (fixture) => {
+      await page.route(fixture.url, async (route) => {
+        await route.fulfill({ json: fixture.response });
+      });
+    })
+  );
+};
 
 test("Root page", async ({ page }) => {
   await page.emulateMedia({ colorScheme: "light" });
@@ -54,4 +68,26 @@ test("Projects page", async ({ page }) => {
   await page.emulateMedia({ colorScheme: "dark" });
 
   await expect(page).toHaveScreenshot("projects-dark.png");
+});
+
+test("Blog comments", async ({ page }) => {
+  setupFixtures(page, [
+    GET_https_public_api_bsky_app_xrpc_app_bsky_feed_getPo_post_2F3ljphoefymc2l_ZS1KSLN,
+  ]);
+
+  await page.emulateMedia({ colorScheme: "light" });
+
+  await page.goto("/posts/jheys-exploding-layers", {
+    waitUntil: "networkidle",
+  });
+
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  await expect(page).toHaveScreenshot("blog-comments-light.png");
+
+  await page.emulateMedia({ colorScheme: "dark" });
+
+  await expect(page).toHaveScreenshot("blog-comments-dark.png");
 });
